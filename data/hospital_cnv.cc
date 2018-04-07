11f852c2f4375792808bde231a49e9da6f8b0f42
沖 観行 <oki.miyuki@gmail.com>
2018-04-08 00:29:26 +0900

    split address with space for geocoding error

--- data/hospital_cnv.cc
+++ data/hospital_cnv.cc
@@ -16,6 +16,19 @@ std::string toVaccineNumbers( const std::vector<std::string>& elms ) {
 	return boost::algorithm::join( vaccs, ":" );
 }
 
+std::string splitAddress( std::string& address ) {
+	std::string result;
+	size_t n = address.find_first_of(" ");
+	if( n != std::string::npos ) {
+		result = address.substr( 0, n );
+		address = address.substr( n );
+	} else {
+		result = address;
+		address = "";
+	}
+	return result;
+}
+
 int main() {
   std::string line;
 	std::ifstream ifs;
@@ -24,7 +37,9 @@ int main() {
 	  std::vector<std::string> elms; 
 		std::getline( ifs, line );
 		boost::split( elms, line, boost::is_any_of(","));
-		std::cout << elms[0] << "," << elms[1] << "," << elms[2] << elms[3] << elms[4] << "," << elms[5] << "-" << elms[6] << "," << toVaccineNumbers( elms ) << std::endl;
+		std::string address = elms[2] + elms[3] + elms[4];
+		std::string new_address = splitAddress( address );
+		std::cout << elms[0] << "," << elms[1] << "," << new_address << "," << address << "," << elms[5] << "-" << elms[6] << "," << toVaccineNumbers( elms ) << std::endl;
 	}
 	return 0;
 }
